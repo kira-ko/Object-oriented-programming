af72635fca120515f03832f7a338dddf66be8d01
Kira <kirrzi.k@gmail.com>
2026-01-09 00:22:24 +0300

    Added lab2

--- Lab1VendingMachine/obj/Debug/net9.0/Lab1VendingMachine.AssemblyInfo.cs
+++ Lab1VendingMachine/obj/Debug/net9.0/Lab1VendingMachine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1VendingMachine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282986f9f63016db17b6de0b0ddd3fc2734bf9a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1VendingMachine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1VendingMachine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
